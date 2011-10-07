0240cbdd89691d43201f0aa642cc5c64bffff797
Remy Loubradou <remyloubradou@gmail.com>
2011-10-07 19:04:31 +0200

    Added a test to check that the pattern of the Solr Date is followed

---
+++ test/test-UTCDate.js
@@ -0,0 +1,19 @@
+// Dependencies 
+var date = require('./../lib/util/date'),
+   vows = require('vows'),
+   assert = require('assert');
+
+// Suite Test
+
+var suite = vows.describe('Solr Date');
+
+suite.addBatch({
+   'Date submitted by the user being based on Local time.' : {
+      topic : function() {
+         return date.toSolrDateFormat(new Date('Fri Oct 07 2011 18:24:00 GMT+0200 (CEST)'));
+      },
+      'should return a Date following this pattern 1995-12-31T23:59:59.999Z and based on UTC time' : function(date){
+        assert.equal(date,'2011-10-07T16:24:00.0Z');
+      }
+   }
+}).export(module);

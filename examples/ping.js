738a857d3fda8f82ce83bab8e9e132f19a45b5d8
Remy Loubradou <remyloubradou@gmail.com>
2012-01-20 23:39:06 +0100

    NEW: Add an example of the use of the ping command.

---
+++ examples/ping.js
@@ -0,0 +1,9 @@
+// Dependency
+var solr = require('./../lib/solr');
+
+var client = solr.createClient();
+var callback = function(err,res){
+   if(err) console.log(err);
+   if(res) console.log(res);
+}
+client.ping(callback);

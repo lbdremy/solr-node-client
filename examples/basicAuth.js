1083258427485c0841ec60303b1a8a9b96e0d8e6
Remy Loubradou <remyloubradou@gmail.com>
2012-01-22 00:47:12 +0100

    NEW: Add example for basic auth.

--- examples/basicAuth.js
+++ examples/basicAuth.js
@@ -2,7 +2,7 @@
 var solr = require('./../lib/solr');
 
 var client = solr.createClient();
-client.basicAuth('admi','passtest');
+client.basicAuth('admin','passtest');
 console.log('Authorization header: ' + client.options.authorization);
 //client.unauth();
 console.log(client.options.authorization ? 'Still auth.' : 'Unauth');
@@ -14,3 +14,4 @@ var callback = function(err,res){
 
 var query = client.createQuery().q('laptop').dismax().qf({title_t : 0.2 , description_t : 3.3}).mm(2).start(0).rows(10);
 client.query(query,callback);
+

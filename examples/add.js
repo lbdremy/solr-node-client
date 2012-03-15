7b87547701d24660b9d6c9895a10e17eaced5a62
Remy Loubradou <remyloubradou@gmail.com>
2012-03-15 17:47:02 +0100

    NEW: Add comments to explain important LOC.

--- examples/add.js
+++ examples/add.js
@@ -1,21 +1,37 @@
- var solr = require('./../lib/solr');
+/**
+ * Load dependency
+ */
  
+var solr = require('./../lib/solr');
 
+// Create a client
 var client = solr.createClient();
 
-var callback = function(err,res){
-   if(err) console.log('Error:' + err);
-   if(res) console.log('res:' + res);
+// function executed when the Solr server responds
+var callback = function(err,json){
+   if(err){
+      console.log(err);
+   }else{
+      console.log('JSON response:' + json);
+   }
 }
+
+// Auto commit document added.
 client.autoCommit = true;
+
+// Send a request every time there are 4 or more documents added with the function add()
 client.updateEach = 4;
-for(var i = 0; i <= 2 ; i++){
+
+var docs = [];
+for(var i = 0; i <= 10 ; i++){
    var doc = {
        id : 82893 + i,
        title : "Title "+ i,
        description : "Text"+ i + "Alice"
    }
-   client.add(doc,callback);
+   docs.push(doc);
 }
 
-client.purgeAdd(callback);
+// Add documents and flush added documents
+client.add(docs,callback);
+client.flushAdd(callback);

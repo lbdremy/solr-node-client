49a4e23bd4cedaac9a554b790ecba7280882aa35
David Schlechtweg <david.schlechtweg@b-s-s.de>
2013-04-02 19:24:27 +0200

    adding initial support of range facets.

--- test/mocks.js
+++ test/mocks.js
@@ -205,6 +205,18 @@ exports.facet = function ( nock ) {
     .reply ( 200, "{\"responseHeader\":{\"status\":0,\"QTime\":12,\"params\":{\"facet.missing\":\"false\",\"facet\":\"true\",\"facet.mincount\":\"0\",\"facet.offset\":\"0\",\"facet.limit\":\"20\",\"wt\":\"json\",\"facet.method\":\"fc\",\"rows\":\"0\",\"facet.sort\":\"count\",\"facet.query\":\"title:Ipad\",\"q\":\"*:*\",\"facet.prefix\":\"Ipa\",\"facet.field\":\"title\"}},\"response\":{\"numFound\":10,\"start\":0,\"docs\":[]},\"facet_counts\":{\"facet_queries\":{\"title:Ipad\":0},\"facet_fields\":{\"title\":[]},\"facet_dates\":{},\"facet_ranges\":{}}}", { date: 'Sun, 06 May 2012 22:11:56 GMT',
     'content-type': 'application/json; charset=UTF-8',
     connection: 'close',
+    server: 'Jetty(7.5.3.v20111011)' } )
+
+    .get ( '/solr/select?q=*:*&rows=0&facet=true&facet.range=price&f.price.facet.range.start=0&f.price.facet.range.end=100&f.price.facet.range.gap=10&f.price.facet.range.hardend=false&wt=json' )
+    .reply ( 200, "{\"responseHeader\":{\"status\":0,\"QTime\":12,\"params\":{\"facet.missing\":\"false\",\"facet\":\"true\",\"facet.mincount\":\"0\",\"facet.offset\":\"0\",\"facet.limit\":\"20\",\"wt\":\"json\",\"facet.method\":\"fc\",\"rows\":\"0\",\"facet.sort\":\"count\",\"facet.query\":\"title:Ipad\",\"q\":\"*:*\",\"facet.prefix\":\"Ipa\",\"facet.field\":\"title\"}},\"response\":{\"numFound\":10,\"start\":0,\"docs\":[]},\"facet_counts\":{\"facet_queries\":{\"title:Ipad\":0},\"facet_fields\":{\"title\":[]},\"facet_dates\":{},\"facet_ranges\":{}}}", { date: 'Sun, 06 May 2012 22:11:56 GMT',
+    'content-type': 'application/json; charset=UTF-8',
+    connection: 'close',
+    server: 'Jetty(7.5.3.v20111011)' } )
+
+    .get ( '/solr/select?q=*:*&rows=0&facet=true&facet.range=price&f.price.facet.range.start=0&f.price.facet.range.end=100&f.price.facet.range.gap=10&f.price.facet.range.hardend=false&facet=true&facet.range=screensize&f.screensize.facet.range.start=0&f.screensize.facet.range.end=55&f.screensize.facet.range.gap=5&f.screensize.facet.range.hardend=false&wt=json' )
+    .reply ( 200, "{\"responseHeader\":{\"status\":0,\"QTime\":12,\"params\":{\"facet.missing\":\"false\",\"facet\":\"true\",\"facet.mincount\":\"0\",\"facet.offset\":\"0\",\"facet.limit\":\"20\",\"wt\":\"json\",\"facet.method\":\"fc\",\"rows\":\"0\",\"facet.sort\":\"count\",\"facet.query\":\"title:Ipad\",\"q\":\"*:*\",\"facet.prefix\":\"Ipa\",\"facet.field\":\"title\"}},\"response\":{\"numFound\":10,\"start\":0,\"docs\":[]},\"facet_counts\":{\"facet_queries\":{\"title:Ipad\":0},\"facet_fields\":{\"title\":[]},\"facet_dates\":{},\"facet_ranges\":{}}}", { date: 'Sun, 06 May 2012 22:11:56 GMT',
+    'content-type': 'application/json; charset=UTF-8',
+    connection: 'close',
     server: 'Jetty(7.5.3.v20111011)' } );
 
 }

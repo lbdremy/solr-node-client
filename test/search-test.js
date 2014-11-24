ef0a6f95bfceb8739232905859b1ab5c7807dd2a
cbollerud <bollo@usa.com>
2014-11-24 10:37:03 -0800

    Change search to allow the use of POST instead of GET.  Requests will default to GET.  Set "searchMethod: 'POST'" to use POST in place of GET requests.

--- test/search-test.js
+++ test/search-test.js
@@ -17,7 +17,7 @@ var basePath = [config.client.path, config.client.core].join('/').replace(/\/$/,
 
 describe('Client',function(){
 	describe('#search("q=*:*")',function(){
-		it('should find all documents',function(done){
+		it('should find all documents using GET',function(done){
 			client.search('q=*:*',function(err,data){
 				sassert.ok(err,data);
 				assert.deepEqual({ q : '*:*' , wt: 'json' },data.responseHeader.params);
@@ -26,7 +26,31 @@ describe('Client',function(){
 		});
 	});
 	describe('#search(query)',function(){
-		it('should find documents describe in the `query` instance of `Query`',function(done){
+		it('should find documents using GET described in the `query` instance of `Query`',function(done){
+			var query = client.createQuery()
+				.q({
+					'title_t' : 'test'
+				});
+			client.search(query,function(err,data){
+				sassert.ok(err,data);
+				assert.deepEqual({ q : 'title_t:test', wt: 'json' },data.responseHeader.params);
+				done();
+			});
+		});
+	});
+	describe('#search("q=*:*")',function(){
+		it('should find all documents using POST',function(done){
+			client.options.searchMethod = 'POST'
+			client.search('q=*:*',function(err,data){
+				sassert.ok(err,data);
+				assert.deepEqual({ q : '*:*' , wt: 'json' },data.responseHeader.params);
+				done();
+			});
+		});
+	});
+	describe('#search(query)',function(){
+		it('should find documents using POST, described in the `query` instance of `Query`',function(done){
+			client.options.searchMethod = 'POST'
 			var query = client.createQuery()
 				.q({
 					'title_t' : 'test'

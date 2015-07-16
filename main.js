237ecaec2a00bac2e5b810712f456515b1e432d7
Benson <bsui.strade@gmail.com>
2015-07-16 11:05:33 -0500

    new

--- main.js
+++ main.js
@@ -5,6 +5,17 @@
  * MIT Licensed
  */
 
-module.exports = exports = require("./lib/solr");
+//module.exports = exports = require("./lib/solr");
 
 
+var solr = require('./lib/new');
+var client = solr.createClient({
+  endpoints: ['10.64.30.11:8983', '10.64.30.12:8983'],
+  core:'user'
+});
+
+var query = client.createQuery().q({email:'lli.strade@gmail.com'});
+
+client.search(query, function(err, obj){
+  console.log(err, obj);
+});

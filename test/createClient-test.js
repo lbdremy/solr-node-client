f8a07e60acfaa5e23f44205b340c0cf63e8a7946
Benson <bsui.strade@gmail.com>
2015-07-17 09:23:19 -0500

    create client test passed

--- test/createClient-test.js
+++ test/createClient-test.js
@@ -3,53 +3,72 @@
  */
 
 var mocha = require('mocha'),
-	assert = require('chai').assert,
-	libPath = process.env['SOLR_CLIENT_COV'] ? '../lib-cov' : '../lib',
-	solr = require( libPath + '/solr');
+  assert = require('chai').assert,
+  libPath = process.env['SOLR_CLIENT_COV'] ? '../lib-cov' : '../lib',
+  solr = require(libPath + '/solr'),
+  https = require('https');
 
 // Test suite
 
-describe('solr',function(){
-	describe('.createClient()',function(){
-		it('should return a `Client` instance',function(){
-			var client = solr.createClient();
-			assert.equal(client.options.host,'127.0.0.1');
-            assert.equal(client.options.port,8983);
-            assert.equal(client.options.core,'');
-            assert.equal(client.options.path,'/solr');
-		});
-	});
-	describe('.createClient({ host : "10.10.10.10",port : 8080, core : "core1", path : "/solr4"})',function(){
-		it('should return a `Client` instance with the given options',function(){
-			var options = {
-				host : '10.10.10.10',
-				port : 8080,
-				core : 'core1',
-				path : '/solr4'
-			};
-			var client = solr.createClient(options);
-			assert.equal(client.options.host,'10.10.10.10');
-            assert.equal(client.options.port,8080);
-            assert.equal(client.options.core,'core1');
-            assert.equal(client.options.path,'/solr4');
-		});
-	});
-	describe('.createClient("10.10.10.10",8080,"core1","/solr4")',function(){
-		it('should return a `Client` instance with the given options',function(){
-			var client = solr.createClient('10.10.10.10',8080,'core1','/solr4');
-			assert.equal(client.options.host,'10.10.10.10');
-            assert.equal(client.options.port,8080);
-            assert.equal(client.options.core,'core1');
-            assert.equal(client.options.path,'/solr4');
-		});
-	});
-	describe('.createClient({ secure : true })', function(){
-		it('should create a `Client` instance with secure set to true', function(){
-			var options = {
-				secure : true
-			};
-			var client = solr.createClient(options);
-			assert.isTrue(client.options.secure);
-		});
-	});
+describe('solr', function() {
+  describe('.createClient()', function() {
+    it('should return a `Client` instance', function() {
+      var client = solr.createClient();
+      assert.equal(client.options.endpoints.length, 1);
+      assert.equal(client.options.endpoints[0], '127.0.0.1:8983');
+      assert.equal(client.options.core, '');
+      assert.equal(client.options.path, '/solr');
+    });
+  });
+  describe('.createClient({ endpoints : "10.10.10.10:8080", core : "core1", path : "/solr4"})', function() {
+    it('should return a `Client` instance with the given options', function() {
+      var options = {
+        endpoints: ['10.10.10.10:8080'],
+        core: 'core1',
+        path: '/solr4'
+      };
+      var client = solr.createClient(options);
+      assert.equal(client.pool.nodes.length, 1);
+      assert.equal(client.pool.nodes[0].ip,'10.10.10.10');
+      assert.equal(client.pool.nodes[0].port,'8080');
+      assert.equal(client.options.core, 'core1');
+      assert.equal(client.options.path, '/solr4');
+    });
+  });
+  describe('.createClient(["10.10.10.10:8080"],"core1","/solr4")', function() {
+    it('should return a `Client` instance with the given options', function() {
+      var client = solr.createClient(['10.10.10.10:8080'], 'core1', '/solr4');
+      assert.equal(client.pool.nodes.length, 1);
+      assert.equal(client.pool.nodes[0].ip,'10.10.10.10');
+      assert.equal(client.pool.nodes[0].port,'8080');
+      assert.equal(client.options.core, 'core1');
+      assert.equal(client.options.path, '/solr4');
+    });
+  });
+  describe('.createClient({ secure : true })', function() {
+    it('should create a `Client` instance with secure set to true', function() {
+      var options = {
+        secure: true
+      };
+      var client = solr.createClient(options);
+      assert.equal(client.pool.http, https);
+    });
+  });
+  describe('.createClient({endpoints:["10.10.10.10:8080", "11.11.11.11:8888"],core:"core1",paht:"/solr4"})', function(){
+    it('should create a `Client` instance with two endpoints', function(){
+      var options = {
+        endpoints: ['10.10.10.10:8080', '11.11.11.11:8888'],
+        core: 'core1',
+        path: '/solr4'
+      };
+      var client = solr.createClient(options);
+      assert.equal(client.pool.nodes.length, 2);
+      assert.equal(client.pool.nodes[0].ip,'10.10.10.10');
+      assert.equal(client.pool.nodes[0].port,'8080');
+      assert.equal(client.pool.nodes[1].ip,'11.11.11.11');
+      assert.equal(client.pool.nodes[1].port,'8888');
+      assert.equal(client.options.core, 'core1');
+      assert.equal(client.options.path, '/solr4');
+    });
+  });
 });

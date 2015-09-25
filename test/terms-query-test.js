7ed8fc1419270a9f31d2ca34ff8125da6f719f1b
Donghyun Go <godong9@gmail.com>
2015-09-25 10:15:38 +0900

    Fix terms-query-test
    - Remove wiki page text
    - Change formatting
    - Change test code

--- test/terms-query-test.js
+++ test/terms-query-test.js
@@ -1,37 +1,4 @@
 // Testing support http://wiki.apache.org/solr/TermsComponent
-
-// Add this setting options to solrconfig.xml
-/*
-
-<searchComponent name="terms" class="solr.TermsComponent"/>
-
-<!-- A request handler for demonstrating the spellcheck component.
-
-  NOTE: This is purely as an example.  The whole purpose of the
-SpellCheckComponent is to hook it into the request handler that
-handles your normal user queries so that a separate request is
-not needed to get suggestions.
-
-  IN OTHER WORDS, THERE IS REALLY GOOD CHANCE THE SETUP BELOW IS
-NOT WHAT YOU WANT FOR YOUR PRODUCTION SYSTEM!
-
-  See http://wiki.apache.org/solr/SpellCheckComponent for details
-  on the request parameters.
--->
-
-  <!-- A request handler for demonstrating the terms component -->
-<requestHandler name="/terms" class="solr.SearchHandler" startup="lazy">
-<lst name="defaults">
-<bool name="terms">true</bool>
-<bool name="distrib">false</bool>
-</lst>
-<arr name="components">
-<str>terms</str>
-</arr>
-</requestHandler>
-*/
-
-
 /**
  * Modules dependencies
  */
@@ -53,31 +20,31 @@ describe('Client#createQuery',function(){
   describe('#terms(options), callback)',function(){
     it('should create a Terms query',function(done){
       var options = {
-        "on": true
-        ,	"fl": 'title'
-        ,	"prefix": 's'
-        , "mincount": 1
-        , "maxcount": 10
-        , "limit": 2
-        , "sort": 'index'
-      };
+            "on": true
+          ,	"fl": 'title'
+          ,	"prefix": 's'
+          , "mincount": 1
+          , "maxcount": 10
+          , "limit": 5
+          , "sort": 'index'
+        };
 
       var query = client.createQuery()
         .terms(options)
         .debugQuery();
 
       client.terms(query, function(err, data){
-        console.log(data);
-        sassert.ok(err, data);
-        assert.deepEqual(data.terms,
+        sassert.ok(err,data);
+        assert.deepEqual(data.responseHeader.params,
           {
-            title:
-              [
-                'smoke',
-                1,
-                'sunrise',
-                1
-              ]
+            terms: 'true',
+            wt: 'json',
+            'terms.fl': 'title',
+            'terms.prefix': 's',
+            'terms.mincount': '1',
+            'terms.maxcount': '10',
+            'terms.limit': '5',
+            'terms.sort': 'index'
           });
         done();
       });

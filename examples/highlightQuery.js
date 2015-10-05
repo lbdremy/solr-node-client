8becec6b27a9d735429006214e9c36ec6fee2722
Luke Taverne <luketaverne@gmail.com>
2015-10-05 16:15:03 +0200

    docs(example): query highlighting example
    Added minimal example for the Standard Query Highlighter

---
+++ examples/highlightQuery.js
@@ -0,0 +1,20 @@
+// Use `var solr = require('solr-client')` in your code
+var solr = require('./../lib/solr');
+
+var client = solr.createClient();
+var query = client.createQuery()
+               .q('*:*')
+               .rows(0)
+               .hl({
+                  on: true,
+                  q : 'title:An Example Title to Highlight',
+                  fl : 'title',
+               });
+
+client.search(query,function(err,obj){
+   if(err){
+      console.log(err);
+   }else{
+      console.log(obj);
+   }
+});

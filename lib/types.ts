49c2d601ead49fe96b5f77f20fbaa7d7e2f0400b
AlinaKul <61979859+AlinaKul@users.noreply.github.com>
2021-08-20 18:30:25 +0300

    Add more types (#269)
    Co-authored-by: Igor Savin <iselwin@gmail.com>

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,25 @@
-export type CallbackFn = (
-  error: Error,
-  result: any
-) => void
+import type { Agent as HttpAgent} from 'http';
+import type { Agent as HttpsAgent} from 'https';
+
+export type CallbackFn = (error: Error, result: any) => void;
+
+export type Options = {
+  parameters?: Record<string, any>;
+  format?: string;
+  contentType?: string;
+  path: string;
+};
+
+export type Params = {
+  host: string;
+  port: string | number;
+  core?: string;
+  authorization: string;
+  fullPath: string;
+  secure: boolean;
+  bigint: boolean;
+  agent: HttpAgent | HttpsAgent;
+  request: Record<string, any>;
+  ipVersion: number;
+  params?: string;
+};

c29fdd6a6018ae89d3be590a55f3947ac4fca3b4
AlinaKul <61979859+AlinaKul@users.noreply.github.com>
2021-08-23 23:36:06 +0300

    Add TS types for Query (#271)

--- lib/types.ts
+++ lib/types.ts
@@ -52,3 +52,103 @@ export type FullSolrClientParams = {
   get_max_request_entity_size: boolean | number
   authorization?: string
 }
+
+export type DateOptions = {
+  field: string
+  start: string | number | Date
+  end: string | number | Date
+}
+
+export type Filters = {
+  field: string
+  value: string | number | Date
+}
+export type GroupOptions = {
+  on?: boolean
+  field?: string | string[]
+  query?: Record<string, any> | Record<string, any>[]
+  limit?: number
+  offset?: number
+  sort?: string
+  format?: string
+  main?: boolean
+  ngroups?: boolean
+  truncate?: boolean
+  cache?: number
+}
+
+export type FacetOptions = {
+  on?: boolean
+  query?: string
+  field: string | string[]
+  prefix?: string
+  sort?: string
+  limit?: number
+  offset?: number
+  mincount?: number
+  missing?: boolean
+  method?: string
+  pivot: Pivot
+}
+export type Pivot = {
+  mincount?: string
+  fields
+}
+
+export type MltOptions = {
+  on?: boolean
+  fl?: string | Record<string, any>[]
+  count?: number
+  mintf?: number
+  mindf?: number
+  minwl?: number
+  maxwl?: number
+  maxqt?: number
+  maxntp?: number
+  boost?: boolean
+  qf?: string | Record<string, any>
+}
+
+export type HlOptions = {
+  on?: boolean
+  q?: Record<string, any>
+  qparser?: string
+  fl?: Record<string, any>
+  snippets?: number
+  fragsize?: number
+  mergeContiguous?: boolean
+  maxAnalyzedChars?: number
+  maxMultiValuedToExamine?: number
+  maxMultiValuedToMatch?: number
+  alternateField?: string
+  maxAlternateFieldLength?: number
+  formatter?: string
+  simplePre?: string
+  simplePost?: string
+  fragmenter?: string
+  highlightMultiTerm?: boolean
+  requireFieldMatch?: boolean
+  usePhraseHighlighter?: boolean
+  regexSlop?: number
+  regexPattern?: string
+  regexMaxAnalyzedChars?: number
+  preserveMulti?: boolean
+  payloads?: boolean
+}
+
+export type TermsOptions = {
+  on?: boolean
+  fl: string
+  lower?: string
+  lowerIncl?: boolean
+  mincount?: number
+  maxcount?: number
+  prefix?: string
+  regex?: string
+  regexFlag?: string
+  limit?: number
+  upper?: string
+  upperIncl?: boolean
+  raw?: boolean
+  sort?: string
+}

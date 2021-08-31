989f09b12d0b8ebf5c37347bc201d63324f1e0d0
dolf <rudolfbyker@gmail.com>
2021-08-31 09:33:45 +0200

    Convert `require` to `import` in update-test.ts

--- test/update-test.ts
+++ test/update-test.ts
@@ -7,7 +7,6 @@ const figc = require('figc'),
   solr = require(libPath + '/solr');
 import * as sassert from './sassert';
 
-// Test suite
 const config = figc(__dirname + '/config.json');
 const client = solr.createClient(config.client);
 [config.client.path, config.client.core].join('/').replace(/\/$/, '');

cba686d8f4f31ad246529e855d0fe6a0d4b9e3be
Alina Kul <alinavilcin@gmail.com>
2021-09-30 10:38:25 +0300

    refactor commit tests

--- test/ping-test.ts
+++ test/ping-test.ts
@@ -1,7 +1,6 @@
 import { assert } from 'chai';
 import * as figc from 'figc';
 import { createClient } from '../lib/solr';
-import * as sassert from './sassert';
 import { dataOk } from './sassert';
 
 const config = figc(__dirname + '/config.json');

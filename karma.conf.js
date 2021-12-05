ba00c50a6d97f54650412054e5accc336e4dc1ff
Andre Willomitzer <andre.willomitzer@gmail.com>
2021-12-05 11:18:44 -0500

    added typescript pre-processing and 'lib' pattern

--- karma.conf.js
+++ karma.conf.js
@@ -1,19 +1,17 @@
 module.exports = function (config) {
   config.set({
-    frameworks: ['jasmine'],
-    files: [
-      {
-        pattern: 'test/**/*.ts',
-        type: 'js'  // to silence the warning. Means load with <script> tag
-      },
-    ],
+    frameworks: ['jasmine', 'karma-typescript'],
+    files: [{ pattern: 'test/**/*.ts' }, { pattern: 'lib/**/*.ts'}],
+    preprocessors: {
+      "**/*.ts": "karma-typescript"
+    },
     reporters: ['progress'],
     port: 9876, // karma web server port
     colors: true,
     logLevel: config.LOG_INFO,
     browsers: ['ChromeHeadless'],
     autoWatch: false,
-    // singleRun: false, // Karma captures browsers, runs the tests and exits
+    singleRun: true, // Karma captures browsers, runs the tests and exits
     concurrency: Infinity,
   });
 };
